78997a22e6f3beddaf3a92c35505a919f0c46697
kailam-ada <kai_lam@hotmail.fr>
2024-04-08 10:47:38 +0200

    hangmanDrawing, hangmanWord, keyboard set

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import { useState } from "react"
 import words from "./wordList.json"
+import HangmanDrawing from "./HangmanDrawing"
+import HangmanWord from "./HangmanWord"
+import Keyboard from "./Keyboard"
 
 function App() {
   const [wordToGuess, setWordToGuess] = useState(() => {
@@ -10,7 +13,23 @@ function App() {
 
   console.log(wordToGuess);
   
-  return <h1>Hi</h1>
+  return (
+    <div 
+    style={{
+      maxWidth: "800px",
+      display: "flex",
+      flexDirection: "column",
+      gap: "2rem",
+      margin: "0 auto",
+      alignItems: "center"}}>
+      <div style={{ fontSize: "2rem", textAlign: "center"}}>Lose Win</div>
+      <HangmanDrawing />
+      <HangmanWord />
+      <div style={{ alignSelf: "stretch"}} >
+        <Keyboard />
+      </div>
+    </div>
+  )
 }
 
 export default App
